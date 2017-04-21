9aa31faa64149dc1c9fd82082e7808469a99f28b
Ziliandil <Devil_Bioshock@mail.ru>
2017-04-21 18:52:23 +0600

    Menuslider

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//alert('Hi'); - мы не будем выводить сообщения через alert()!
+/* это многострочный
+ коммент*/
+console.log('Hi');
+//        console.log('Hi');
+//        console.log('Hi');
+//        console.log('Hi');
+//        console.log('Hi');
+//        console.log('Hi');
+//        console.log('Hi');
+
+var number = 3;
+var myString = 'Hi world';
+
+console.log(number);
+console.log('number');
+console.log(myString);
+
+var numberTwo = 2;
+number = number * numberTwo;
+
+console.log(number);
+
+$(document).ready(function(){
+  $('.colorbox').colorbox();
+
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+
